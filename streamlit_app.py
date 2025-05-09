21fd6437d42b6efe96b8c522c0a2b31c2f92c212
awalidya <145305974+awalidya@users.noreply.github.com>
2025-05-09 19:08:18 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -251,31 +251,6 @@ elif tab == "Visualisasi":
         ax.set_xlabel("Klaster")
         ax.set_ylabel("Rata-rata Persentase")
         st.pyplot(fig)
-
-        # Mengubah else menjadi bagian lain untuk menangani kasus ms_final None
-        if ms_final is not None:
-            # Visualisasi 3D
-            st.subheader("Visualisasi Klaster 3D")
-
-            labels = df['cluster_labels']
-            cluster_centers = ms_final.cluster_centers_
-
-            fig = plt.figure(figsize=(10, 6))
-            ax = fig.add_subplot(111, projection='3d')
-
-            ax.scatter(df['sampah_tahunan'], df['pengurangan'], df['penanganan'],
-                       c=labels, cmap='plasma', marker='o', label='Data Points')
-
-            ax.scatter(cluster_centers[:, 0], cluster_centers[:, 1], cluster_centers[:, 2],
-                       s=250, c='blue', marker='X', label='Cluster Centers')
-
-            ax.set_xlabel('Sampah Tahunan')
-            ax.set_ylabel('Pengurangan Sampah')
-            ax.set_zlabel('Penanganan Sampah')
-            ax.legend()
-            st.pyplot(fig)
-        else:
-            st.error("Model belum dimuat atau tidak ditemukan. Silakan jalankan clustering terlebih dahulu.")
     else:
         st.warning("Data belum tersedia atau clustering belum dijalankan.")
 
